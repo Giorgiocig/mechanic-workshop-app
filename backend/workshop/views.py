790f8a397508102df20e0556baa45b593ca2218a
Giorgiocig <giorgiocignitti@gmail.com>
2025-11-25 10:58:53 +0100

    Remove unused imports from client views for cleaner code

--- backend/workshop/views.py
+++ backend/workshop/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework import status, viewsets
 from workshop.models import Client, Vehicule
 from workshop.serializer import ClientSerializer, VehiculeSerializer
