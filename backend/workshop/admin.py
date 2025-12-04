9894d4fea75bc72cf9e95ca952ad703cbd9762ec
Giorgiocig <giorgiocignitti@gmail.com>
2025-12-04 11:50:42 +0100

    Add Vehicule to admin file to make it visible in the admin panel

--- backend/workshop/admin.py
+++ backend/workshop/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Client
+from .models import Client, Vehicule
 
 admin.site.register(Client)
+admin.site.register(Vehicule)
